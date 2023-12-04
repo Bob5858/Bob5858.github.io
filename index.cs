534d50448f80c1cc90c55d36f7a64cc3a6395379
Bob5858 <152924500+Bob5858@users.noreply.github.com>
2023-12-04 17:43:06 -0500

    Create index.cs

---
+++ index.cs
@@ -0,0 +1,23 @@
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body{
+  height: 100%;
+}
+.content {
+  box-sizing: border-box;
+  min-height: 100%;
+  padding-bottom: 90px;
+    background:yellow;
+  text-align:center;
+  font-size:42px;
+}
+footer {
+  height: 80px;
+  margin-top: -80px;
+  background:red;
+  text-align:center;
+  font-size:42px;
+}
